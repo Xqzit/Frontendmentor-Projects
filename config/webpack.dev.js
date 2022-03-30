b646af48db134a4a1d97c8330ed118ce4bdcd1cd
John Samuel <jhnsml.31@gmail.com>
2022-03-30 13:47:14 +0530

    chore: upgrade deps
    - chore: rewrite configs

---
+++ config/webpack.dev.js
@@ -0,0 +1,37 @@
+const { merge } = require("webpack-merge");
+
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+	mode: "development",
+	devtool: "inline-source-map",
+	devServer: {
+		static: "../dist",
+		open: {
+			app: {
+				name: "chrome",
+			},
+		},
+		hot: true,
+	},
+	watchOptions: {
+		ignored: "**/node_modules/",
+	},
+
+	module: {
+		rules: [
+			// Styles: Inject CSS into the head with source maps
+			{
+				test: /\.css$/,
+				use: [
+					"style-loader",
+					{
+						loader: "css-loader",
+						options: { sourceMap: true, importLoaders: 1, modules: false },
+					},
+					{ loader: "postcss-loader", options: { sourceMap: true } },
+				],
+			},
+		],
+	},
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,42 +1,40 @@
-import '../css/style.css';
-import '../css/page-style.css';
-import elements from './base';
-import { fetchCountries, createIsoMap } from './utils/utils'
-import home from './models/home'
-import filterByRegion from './models/filter'
-import search from './models/search'
-import countryPage from './models/country'
-import { switchTheme } from './views/view';
-
+import "../css/style.css";
+import "../css/page-style.css";
+import elements from "./base";
+import { fetchCountries, createIsoMap } from "./utils/utils";
+import home from "./models/home";
+import filterByRegion from "./models/filter";
+import search from "./models/search";
+import countryPage from "./models/country";
+import { switchTheme } from "./views/view";
 
 // Theme Switcher
-elements.themeBtn.addEventListener('click', switchTheme);
+elements.themeBtn.addEventListener("click", switchTheme);
 
 /* Controller */
 const initController = async () => {
-    const data = await fetchCountries();
-    if (data !== undefined) {
-        const isoMap = createIsoMap(data);
-        home(data);
-        filterByRegion(data);
-        search(data);
-        countryPage(data, isoMap);
-    }
+	const data = await fetchCountries();
+	if (data !== undefined) {
+		const isoMap = createIsoMap(data);
+		home(data);
+		filterByRegion(data);
+		search(data);
+		countryPage(data, isoMap);
+	}
 
-    // Click on the logo
-    elements.logo.addEventListener('click', (e) => {
-        home(data);
-        e.preventDefault();
-    });
-}
+	// Click on the logo
+	elements.logo.addEventListener("click", (e) => {
+		home(data);
+		e.preventDefault();
+	});
+};
 
 initController();
-
 
 // Things to do
 
 /*
-* Add Pagination
-* https://www.thatsoftwaredude.com/content/9101/custom-javascript-pagination-of-objects
-* https://www.thatsoftwaredude.com/content/6125/how-to-paginate-through-a-collection-in-javascript
-*/
+ * Add Pagination
+ * https://www.thatsoftwaredude.com/content/9101/custom-javascript-pagination-of-objects
+ * https://www.thatsoftwaredude.com/content/6125/how-to-paginate-through-a-collection-in-javascript
+ */
